c82c041835110b838288de314f6075f2263adf37
alexsamuel <sha90@sfu.ca>
2012-12-02 01:13:54 -0800

    add a test tetris/comet page

---
+++ src/tetris.erl
@@ -0,0 +1,49 @@
+%% -*- mode: nitrogen -*-
+-module (tetris).
+-compile(export_all).
+-include_lib("nitrogen_core/include/wf.hrl").
+-include("records.hrl").
+
+main() -> #template { file="./site/templates/tetris.html" }.
+
+
+title() -> "Href Tetris".
+headline() -> "Href Tetris-Comet test".
+
+
+body() -> 
+
+	wf:comet_global(fun() -> comet_loop() end, my_pool),
+    [
+	#p{},
+        #button{id=action,postback=some_event,text="Test"},
+	#button{id=action,postback=comet_event,text="Send"},
+	#panel{ id=placeholder,class=placeholder},
+	#p{}
+        
+    ].
+	
+
+event(some_event) ->
+	wf:wire("test++"), %%Increatment test
+	wf:wire("textfield.textContent = test");
+
+
+event(comet_event) ->
+
+    wf:send_global(my_pool, {message, "Clicked"});
+
+event(_) -> ok.
+
+comet_loop() ->
+    receive 
+	{message, "Clicked"} ->
+            wf:wire("test = test+10"),
+	    wf:wire("textfield.textContent = test"),
+	    wf:insert_bottom(placeholder, "Clicked"),
+	wf:wire("obj('placeholder').scrollTop = obj('placeholder').scrollHeight;"),
+
+	wf:flush()
+    end,
+comet_loop().  
+
