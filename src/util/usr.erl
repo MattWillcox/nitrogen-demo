376fcd7e1e23e7c8d240f3b4b7cc92f5ee60c5ea
tonywsshim <tony.ws.shim@gmail.com>
2012-12-02 17:18:22 -0800

    Added password reset feature
    It can now reset the password for a user. Still need some kind of
    security, such as asking for a security question.

--- src/util/usr.erl
+++ src/util/usr.erl
@@ -22,3 +22,10 @@ changePassword(Usr,Password,NewPassword) ->
 		Exists -> db:q("update players SET password=? where user =?",[NewPassword,Usr]);
 		true -> {error, "Ooops, you may have made mistakes typing in your id or password. Please try again."}
 	end.
+
+resetPassword(Usr, Password) ->
+	Exists = db:qexists("select * from players where user =?",[Usr]),
+	if
+		Exists -> db:q("update players SET password=? where user =?",[Password,Usr]);
+		true -> {error, "User name does not exist"}
+	end.
