597253a84b0fa758a4cc936f46ff2e5337cd1762
tonywsshim <tony.ws.shim@gmail.com>
2012-12-04 12:18:19 -0800

    Fixed error after merging

--- src/beta.erl
+++ src/beta.erl
@@ -18,39 +18,42 @@ Usr = case wf:user() of
 
 Body = [
 
-#container_12 { body = [
+    #container_12 { body = [
         #grid_12 { style = "border: 10px solid grey; text-align: center;", body = [ 
                 #singlerow {style="width: 920px;", cells=[
-            #tablecell { align=center, body=#link { text="Home", url = "/beta" }},
-            #tablecell { align=center, body=#link { text="Leaderboard", url = "/leaderboard" }},
-            #tablecell { align=center, body=#link { text="Profile", url = "/profile"}},
-            #tablecell { align=center, body=#link { text="Friends", url = "/beta" }},
-            #tablecell { align=center, body=#link { text="Chat", url = "/chat" }},
-            #tablecell { align=center, body=#link { text="Logout", postback = logout }}
-            ]},
+                    #tablecell { align=center, body=#link { text="Home", url = "/beta" }},
+                    #tablecell { align=center, body=#link { text="Leaderboard", url = "/leaderboard" }},
+                    #tablecell { align=center, body=#link { text="Profile", url = "/profile"}},
+                    #tablecell { align=center, body=#link { text="Friends", url = "/beta" }},
+                    #tablecell { align=center, body=#link { text="Chat", url = "/chat" }},
+                    #tablecell { align=center, body=#link { text="Logout", postback = logout }}
+                    ]
+                }
+            ]
+        },
         #grid_clear{},
         #grid_12 { body = [
             #panel { style = "text-align:right;", body = [#span{ text = "Currently logged in as: "},
-            #span { id = currentUser, text  = CurrentUseir}]}]},
+            #span { id = currentUser, text  = Usr}]}]},
         #grid_clear{},    
         #grid_4 { style = "font-size: small;", alpha= true, omega = true, body = [
-        #image { style = "width: 90%", image = "/images/logo.jpg"},
+            #image { style = "width: 90%", image = "/images/logo.jpg"},
         
-        [
-        "Instructions:
-        Login to start searching for an opponent.
-        Your statistics are stored under the
-        'Profile' link at the top. 
-        If you do not have account yet,
-        create an account to the right of the game screen.
-        <p>
-        Controls:
-        Use the arrows keys to move the pieces. 
-        Left and right move the pieces sideways, 
-        the Up arrow will rotate itand the down 
-        arrow will move your piece down faster.
-        "
-        ]
+            [
+            "Instructions:
+            Login to start searching for an opponent.
+            Your statistics are stored under the
+            'Profile' link at the top. 
+            If you do not have account yet,
+            create an account to the right of the game screen.
+            <p>
+            Controls:
+            Use the arrows keys to move the pieces. 
+            Left and right move the pieces sideways, 
+            the Up arrow will rotate itand the down 
+            arrow will move your piece down faster.
+            "
+            ]
 
         ]},
         #grid_4 { id = game1, style = "border: 10px solid black; color: white; 
