ceaa6355e2955a9d864db107ddf7b7cb49b3eb07
uFish <mps9@sfu.ca>
2012-12-05 14:40:08 -0800

    altered user table

--- src/util/sql.sql
+++ src/util/sql.sql
@@ -3,3 +3,6 @@ CREATE TABLE game (gid int NOT NULL AUTO_INCREMENT, user1 varchar(25), user2 var
 CREATE TABLE user (uid int NOT NULL AUTO_INCREMENT, email varchar(255), username varchar(25), password varchar(32), wins int, losses int, elo int, SecretQuestionAnswer varchar(255), PRIMARY KEY (uid));
 CREATE TABLE friend (fid int NOT NULL AUTO_INCREMENT, user1 varchar(25), user2 varchar(25), status BOOLEAN, PRIMARY KEY(fid));
 
+INSERT INTO game (user1, user2, elo1, elo2, winner) VALUES ('user1','user2',1200,1250,'user1');
+
+ALTER TABLE user ADD lastvisit timestamp;
