e488731967cc6cf35b75df57efff50a314c148cb
uFish <mps9@sfu.ca>
2012-12-05 14:23:08 -0800

    elo adjustment function

--- src/util/elo.erl
+++ src/util/elo.erl
@@ -5,7 +5,23 @@
 top10() ->
 	db:q("select username,wins,losses,elo from user ORDER BY elo DESC LIMIT 0,10").
 
-calcElos(Player, Elo) ->
+quickSum(L) -> quickSum(L, 0).
+quickSum([H|T], Sum) -> quickSum(T, Sum + H);
+quickSum([], Sum)    -> Sum.
 
+calcNewElo({Player,BaseElo},Opponents,Winner) ->
+	case {Player,BaseElo} of
+		Winner -> Sa = 1;
+		_ -> Sa = 0
+	end,
+	K = 15,
+	Set = lists:map(fun({_Player,Elo}) ->
+		K*(Sa-(1/(1+math:pow(10,(Elo-BaseElo)/400))))end, Opponents),
+	BaseElo + quickSum(Set).
 
-adjustElo({Player,ELO}, Winner) ->
+adjustElos(PlayerElos,Winner) ->
+	lists:map(fun({Player,Elo}) ->
+  		Opponents = [{P,E} || {P,E} <- PlayerElos, P=/=Player],
+ 		NewElo = calcNewElo({Player,Elo},Opponents,Winner),
+ 		{Player,NewElo} end, PlayerElos). 
+
