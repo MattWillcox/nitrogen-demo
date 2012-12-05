66e406e0ce1bac9b4b80f1a1353cd6b1a0cc67e5
uFish <mps9@sfu.ca>
2012-12-04 18:08:42 -0800

    slight change to table name

--- src/util/sql.sql
+++ src/util/sql.sql
@@ -1,4 +1,5 @@
 -- SQL
 CREATE TABLE game (gid int NOT NULL AUTO_INCREMENT, user1 varchar(25), user2 varchar(25), elo1 int, elo2 int, winner varchar(25), keystrokes1 int, keystrokes2 int, length int, time timestamp, PRIMARY KEY (gid));
 CREATE TABLE user (uid int NOT NULL AUTO_INCREMENT, email varchar(255), username varchar(25), password varchar(32), wins int, losses int, elo int, SecretQuestionAnswer varchar(255), PRIMARY KEY (uid));
-CREATE TABLE friends (fid int NOT NULL AUTO_INCREMENT, user1 varchar(25), user2 varchar(25), status BOOLEAN, PRIMARY KEY(fid));
+CREATE TABLE friend (fid int NOT NULL AUTO_INCREMENT, user1 varchar(25), user2 varchar(25), status BOOLEAN, PRIMARY KEY(fid));
+
